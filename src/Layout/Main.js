4f86980ff6ddc62d711843d0a938b85021ec0e1b
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-01-10 13:32:41 +0600

    Projects setup react  react router dom tailwind css  desiy ui and create folder router and private router

---
+++ src/Layout/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+     return (
+          <div>
+               <p> This is main page need to create components and set in this page</p>
+          </div>
+     );
+};
+
+export default Main;
